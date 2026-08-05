9f741b89489013f76d8a9edfbdb6a531510efa21
Ishara Madusanka <ishara.madusanka@dialog.lk>
2026-08-05 15:35:43 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/assemble-connect-portal/", // <-- Add this line for GitHub Pages deployment!
+  base: "/", // <-- Add this line for GitHub Pages deployment!
   server: {
     host: "::",
     port: 8080,
